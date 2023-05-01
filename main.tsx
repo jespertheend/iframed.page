90c2f059e5d8c319b2a697de30ac76c67192ee88
Jesper van den Ende <jespertheend@gmail.com>
2023-05-01 23:02:32 +0200

    make url field work

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
-import { serve } from "https://deno.land/std@0.185.0/http/mod.ts";
-import { h, renderSSR, jsx } from "https://deno.land/x/nano_jsx@v0.0.37/mod.ts";
+import { serve, Status } from "https://deno.land/std@0.185.0/http/mod.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.37/mod.ts";
 import { processString } from "https://esm.sh/uglifycss@0.0.29";
 
 const dev = Deno.args.includes("--dev");
@@ -7,11 +7,20 @@ const dev = Deno.args.includes("--dev");
 serve(async (request) => {
 	const url = new URL(request.url);
 
-	let content: jsx;
+	let content;
 	if (url.pathname == "/") {
+		const page = url.searchParams.get("page");
+		if (page) {
+			return new Response(null, {
+				status: Status.Found,
+				headers: {
+					Location: "/" + page,
+				},
+			});
+		}
 		content = () => (
-			<form>
-				<input></input>
+			<form action="/" method="get">
+				<input name="page" placeholder="Enter a URL"></input>
 				<button type="submit">Go</button>
 			</form>
 		);
@@ -24,6 +33,16 @@ serve(async (request) => {
 		});
 	} else {
 		const src = url.pathname.substring(1);
+		try {
+			new URL(src);
+		} catch {
+			return new Response(null, {
+				status: Status.Found,
+				headers: {
+					Location: "/https://" + src,
+				},
+			});
+		}
 		content = () => (
 			<div id="iframeContainer">
 				<iframe src={src}></iframe>
